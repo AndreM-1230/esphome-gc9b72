9bc333ddc3da9c057fa14199714bb457040cab9e
Andre <ymottnaf@yandex.ru>
2026-08-17 20:49:23 +0300

    upd

--- components/gc9b72/display.py
+++ components/gc9b72/display.py
@@ -47,12 +47,16 @@ async def to_code(config):
         "moononournation/GFX Library for Arduino",
         None,
     )
+    cg.add_library(
+        "MaliosDark/Arduino_GC9B72",
+        None,
+    )
     cg.add_build_flag(
         "-I$PROJECT_PACKAGES_DIR/framework-arduinoespressif32/libraries/SPI/src"
     )
     cg.add_build_flag(
         "-I$PROJECT_PACKAGES_DIR/framework-arduinoespressif32/libraries/Wire/src"
-    )
+    )    
 
     # Безопасно получаем числовое значение rotation из конфига ESPHome
     rotation_val = config[CONF_ROTATION]
